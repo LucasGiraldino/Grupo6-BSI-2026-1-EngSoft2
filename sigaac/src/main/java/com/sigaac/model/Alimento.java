5133432fb441b82748c7d5342b5bbdf7e13fb464
Felipeex <felipeexx48@gmail.com>
2026-05-05 03:53:15 -0300

    feat: adicionado CRUD de alimentos e categoria de alimentos.

--- sigaac/src/main/java/com/sigaac/model/Alimento.java
+++ sigaac/src/main/java/com/sigaac/model/Alimento.java
@@ -1,15 +1,10 @@
 package com.sigaac.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "alimentos")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Alimento {
 
     @Id
@@ -17,7 +12,7 @@ public class Alimento {
     @Column(name = "id_alimento")
     private Integer id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "id_categoria", nullable = false)
     private CategoriaAlimento categoria;
 
@@ -34,6 +29,38 @@ public class Alimento {
     private LocalDate dataVencimento;
 
     @Column(name = "ativo", nullable = false)
-    @Builder.Default
     private Boolean ativo = true;
+
+    public Alimento() {}
+
+    public Alimento(Integer id, CategoriaAlimento categoria, String nome, String descricao, String unidadeMedida, LocalDate dataVencimento, Boolean ativo) {
+        this.id = id;
+        this.categoria = categoria;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.unidadeMedida = unidadeMedida;
+        this.dataVencimento = dataVencimento;
+        this.ativo = ativo;
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public CategoriaAlimento getCategoria() { return categoria; }
+    public void setCategoria(CategoriaAlimento categoria) { this.categoria = categoria; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public String getUnidadeMedida() { return unidadeMedida; }
+    public void setUnidadeMedida(String unidadeMedida) { this.unidadeMedida = unidadeMedida; }
+
+    public LocalDate getDataVencimento() { return dataVencimento; }
+    public void setDataVencimento(LocalDate dataVencimento) { this.dataVencimento = dataVencimento; }
+
+    public Boolean getAtivo() { return ativo; }
+    public void setAtivo(Boolean ativo) { this.ativo = ativo; }
 }
--- sigaac/src/main/java/com/sigaac/model/CategoriaAlimento.java
+++ sigaac/src/main/java/com/sigaac/model/CategoriaAlimento.java
@@ -1,14 +1,9 @@
 package com.sigaac.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
 @Table(name = "categorias_alimentos")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class CategoriaAlimento {
 
     @Id
@@ -21,4 +16,21 @@ public class CategoriaAlimento {
 
     @Column(name = "descricao", columnDefinition = "TEXT")
     private String descricao;
+
+    public CategoriaAlimento() {}
+
+    public CategoriaAlimento(Integer id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
 }
