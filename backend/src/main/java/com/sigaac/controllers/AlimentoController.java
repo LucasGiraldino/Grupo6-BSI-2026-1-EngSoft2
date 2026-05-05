e3a8d2bbd72bf5ef6b17ab4f59576b55b6161645
LucasGiraldino <lucasfilipecamargogiraldino@gmail.com>
2026-05-05 14:50:32 -0300

    fix: corrigir mapeamento API alimentos e categorias para /api

--- backend/src/main/java/com/sigaac/controllers/AlimentoController.java
+++ backend/src/main/java/com/sigaac/controllers/AlimentoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/alimentos")
+@RequestMapping("/api/alimentos")
 public class AlimentoController {
 
     @Autowired
--- backend/src/main/java/com/sigaac/controllers/CategoriaAlimentoController.java
+++ backend/src/main/java/com/sigaac/controllers/CategoriaAlimentoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/alimentos/categorias")
+@RequestMapping("/api/alimentos/categorias")
 public class CategoriaAlimentoController {
 
     @Autowired
